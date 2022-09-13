994d3e547489904c471b25b35470bc23ff772ef7
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-13 13:34:11 +0200

    [gitignore] add predict test and rename fit tests

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -9,17 +9,25 @@ def test_my_linear_regression_default() -> None:
     my_regr = MyLinearRegression()
     assert my_regr.__dict__ == {}
 
-def test_my_linear_regression_r() -> None:
+def test_my_linear_regression_fit_r() -> None:
     my_regr = MyLinearRegression()
     my_regr.fit(x=x_dummy, y=y_dummy)
     assert my_regr.r == 1
 
-def test_my_linear_regression_a() -> None:
+def test_my_linear_regression_fit_a() -> None:
     my_regr = MyLinearRegression()
     my_regr.fit(x=x_dummy, y=y_dummy)
     assert my_regr.a == 5
 
-def test_my_linear_regression_b() -> None:
+def test_my_linear_regression_fit_b() -> None:
     my_regr = MyLinearRegression()
     my_regr.fit(x=x_dummy, y=y_dummy)
     assert my_regr.b == 5
+
+def test_my_linear_regression_predict_b() -> None:
+    my_regr = MyLinearRegression()
+    my_regr.fit(x=x_dummy, y=y_dummy)
+    y_pred = my_regr.predict(x=x_dummy)
+    assert y_pred[0] == 10
+    assert y_pred[1] == 15
+    assert y_pred[2] == 20
