14f8e1640399755ce8d4eb46ffbcf7693cfe48e9
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-13 13:09:56 +0200

    [test] added default test

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,2 +1,5 @@
-import unittest
+from data_science_linear_regression.linear_regression import MyLinearRegression
 
+def test_my_linear_regression_default() -> None:
+    my_regr = MyLinearRegression()
+    print(my_regr.__class__)
