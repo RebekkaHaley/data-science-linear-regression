094501b84d5bc7d3e75f4e69e8ab4a9868517fa5
Rebekka Haley <rebekka.haley@gmail.com>
2022-09-13 13:23:18 +0200

    [unittest] added self attributes

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,5 +1,25 @@
+import numpy as np
+
 from data_science_linear_regression.linear_regression import MyLinearRegression
+
+x_dummy = np.array([1, 2, 3])
+y_dummy = np.array([10, 15, 20])
 
 def test_my_linear_regression_default() -> None:
     my_regr = MyLinearRegression()
-    print(my_regr.__class__)
+    assert my_regr.__dict__ == {}
+
+def test_my_linear_regression_r() -> None:
+    my_regr = MyLinearRegression()
+    my_regr.fit(x=x_dummy, y=y_dummy)
+    assert my_regr.r == 1
+
+def test_my_linear_regression_a() -> None:
+    my_regr = MyLinearRegression()
+    my_regr.fit(x=x_dummy, y=y_dummy)
+    assert my_regr.a == 5
+
+def test_my_linear_regression_b() -> None:
+    my_regr = MyLinearRegression()
+    my_regr.fit(x=x_dummy, y=y_dummy)
+    assert my_regr.b == 5
